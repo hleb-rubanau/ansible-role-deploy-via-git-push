0db481f53999e666ff01ba7e5bd9f0b3d63feb67
Hleb Rubanau <g.rubanau@gmail.com>
2020-02-20 20:55:29 +0300

    more debug

--- templates/deploy.sh
+++ templates/deploy.sh
@@ -5,5 +5,8 @@ set -e
 DEPLOYMENT_WORKDIR="{{ deployment_workdir }}"
 DEPLOYMENT_COMMAND="{{ deployment_command }}"
 
+echo "$0: cd $DEPLOYMENT_WORKDIR" >&2
 cd $DEPLOYMENT_WORKDIR
+
+echo "$0: exec $DEPLOYMENT_COMMAND" >&2 
 exec $DEPLOYMENT_COMMAND 
