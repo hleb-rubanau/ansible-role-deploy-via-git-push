3832fd29f7bdc8b6daeffd9c55aa3dbe430756ee
Hleb Rubanau <g.rubanau@gmail.com>
2020-02-20 04:19:18 +0300

    fix typo

--- templates/hook_script.sh
+++ templates/hook_script.sh
@@ -12,7 +12,7 @@ done
 
 if [ ! -z "$revision_to_checkout" ]; then
   echo "Checking out #revision_to_checkout into $deployment_worktree"
-  git --git-dir "{{ deployment_gitstore }}" --worktree "{{ deployment_worktree }}" checkout -f $revision_to_checkout ;
+  git --git-dir "{{ deployment_gitstore }}" --work-tree "{{ deployment_worktree }}" checkout -f $revision_to_checkout ;
 fi
 
 exec sudo "{{ deployment_script }}"
